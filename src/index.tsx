91e18dce074e097bdd360e7c06766739f41581c3
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-08-06 15:01:40 +0200

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+import SearchComponent from "./components/SearchComponent";
+import MemberComponent from "./components/MemberComponent";
+import MemberDetailsComponent from "./components/MemberDetailsComponent";
+
+const router = createBrowserRouter([
+    {
+        path: "",
+        element: <div><SearchComponent /></div>,
+    },
+    {
+        path: "member",
+        element: <div><MemberComponent /></div>,
+    },
+    {
+        path: "member-details",
+        element: <div><MemberDetailsComponent /></div>,
+    },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
