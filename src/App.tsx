728df77a6aaa2bba8543d3759ef107669fbbb474
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-08-06 15:21:40 +0200

    remove code from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import './App.css';
-import SearchComponent from "./components/SearchComponent";
-import MemberComponent from "./components/MemberComponent";
-import MemberDetailsComponent from "./components/MemberDetailsComponent";
 
 
 function App() {
     return (
-        <div className="App">
-            <div>
-                <SearchComponent />
-            </div>
-            <div>
-                <MemberComponent />
-            </div>
-            <div>
-                <MemberDetailsComponent />
-            </div>
-
-        </div>
+        <div className="App"></div>
     );
 }
 
